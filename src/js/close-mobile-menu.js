ad4b2a6fc581b1fc3a017ac0ff99ab2e2a76af90
Resul Elezi <resul_elezi@protonmail.com>
2025-12-26 21:58:51 +0100

    #

--- src/js/close-mobile-menu.js
+++ src/js/close-mobile-menu.js
@@ -1,2 +1,8 @@
 'use strict';
 
+const hamburgerBtn = document.querySelector('.hs-collapse-toggle');
+
+
+function setAriaExpandedFalse() {
+    hamburgerBtn.setAttribute('aria-expanded', 'false');
+}
