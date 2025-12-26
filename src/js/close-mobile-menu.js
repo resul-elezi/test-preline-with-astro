adc3139520c9191b2f571cacab5d21dad51b5d08
Resul Elezi <resul_elezi@protonmail.com>
2025-12-26 23:49:40 +0100

    #

--- src/js/close-mobile-menu.js
+++ src/js/close-mobile-menu.js
@@ -4,16 +4,21 @@ const hamburgerBtn = document.querySelector('.hs-collapse-toggle');
 const menu = document.querySelector('.hs-collapse');
 const links = document.querySelectorAll('#links-div a');
 
+links.forEach(link =>
+    link.addEventListener('click', () => {
+        if (hamburgerBtn.getAttribute('aria-expanded') === 'true') {
+            hamburgerBtn.click();
+        }
+    })
+);
 
-links.forEach(link => link.addEventListener('click', () => {
-    if (hamburgerBtn.getAttribute('aria-expanded') === 'true') {
-        hamburgerBtn.click();
-    }
-}))
 
 document.documentElement.addEventListener('click', (e) => {
-    if (hamburgerBtn.getAttribute('aria-expanded') === 'true') {
-        if (e.target !== hamburgerBtn && !menu.contains(e.target))
-            hamburgerBtn.click();
+    const isOpen = hamburgerBtn.getAttribute('aria-expanded') === 'true';
+    const clickedInsideMenu = menu.contains(e.target);
+    const clickedOnButton = e.target === hamburgerBtn;
+
+    if (isOpen && !clickedInsideMenu && !clickedOnButton) {
+        hamburgerBtn.click();
     }
 });
