34e8219e82b9eab6204cdda10728d0263f1ac137
Resul Elezi <resul_elezi@protonmail.com>
2026-01-05 06:52:30 +0100

    #

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,10 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
+    },
+    server: {
+        allowedHosts: [
+            ".trycloudflare.com"
+        ]
     }
 });
